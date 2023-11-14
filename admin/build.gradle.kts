3704d88527dedb50d09902a7c3756c785d005db2
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-11-14 14:35:13 +0900

    CHORE: admin 모듈에서 jwt 의존성 추가

--- admin/build.gradle.kts
+++ admin/build.gradle.kts
@@ -10,4 +10,8 @@ dependencies {
     implementation(project(":domain"))
     implementation(project(":common"))
     implementation("org.springframework.boot:spring-boot-starter-security")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
