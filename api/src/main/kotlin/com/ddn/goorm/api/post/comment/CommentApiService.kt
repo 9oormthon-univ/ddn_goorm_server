3395413bbd3a806b9d5d84d5ebdebdf896e88796
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-11-18 22:27:51 +0900

    FEAT: 구름 타래 작성 api

--- api/src/main/kotlin/com/ddn/goorm/api/post/comment/CommentApiService.kt
+++ api/src/main/kotlin/com/ddn/goorm/api/post/comment/CommentApiService.kt
@@ -1,4 +1,19 @@
 package com.ddn.goorm.api.post.comment
 
-class CommentApiService {
+import com.ddn.goorm.api.post.comment.dto.request.CommentCreateReq
+import com.ddn.goorm.domains.group.member.Member
+import com.ddn.goorm.domains.post.comment.Comment
+import com.ddn.goorm.domains.post.comment.CommentDomainService
+import com.ddn.goorm.domains.post.goorm.GoormDomainService
+import org.springframework.stereotype.Service
+
+@Service
+class CommentApiService (
+    private val commentDomainService: CommentDomainService,
+    private val goormDomainService: GoormDomainService
+) {
+    fun createComment(member: Member, req: CommentCreateReq) {
+        val comment: Comment = commentDomainService.createComment(
+            member, goormDomainService.findById(req.goorm), req.contents);
+    }
 }
---
+++ api/src/main/kotlin/com/ddn/goorm/api/post/comment/dto/request/CommentCreateReq.kt
@@ -0,0 +1,3 @@
+package com.ddn.goorm.api.post.comment.dto.request
+
+data class CommentCreateReq(val goorm: Long, val contents: String)
